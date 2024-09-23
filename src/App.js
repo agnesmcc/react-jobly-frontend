a0dc237f67e59384b7d0e6862c925267696d2012
Agnes <agnes.mccall5@gmail.com>
2024-09-22 21:57:53 -0400

    add routing skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import SearchPage from './SearchPage';
+import DetailPage from './DetailPage';
+import LoginPage from './LoginPage';
+import SignupPage from './SignupPage';
+import ProfilePage from './ProfilePage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/companies" element={<SearchPage />} />
+          <Route path="/companies/:handle" element={<DetailPage />} />
+          <Route path="/jobs" element={<SearchPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<div>Page not found</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
