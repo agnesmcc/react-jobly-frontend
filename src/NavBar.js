bc05d4b629b6162667a7e402f0a0ed1fa4b9a5a4
Agnes <agnes.mccall5@gmail.com>
2024-09-25 21:27:34 -0400

    add functionality and styling to navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "reactstrap";
+import "./NavBar.css";
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return (
+        <Navbar expand="md">
+            <NavLink to="/">Jobly</NavLink>
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink to="/companies">Companies</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/jobs">Jobs</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/profile">Profile</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/login">Login</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink to="/signup">Signup</NavLink>
+                </NavItem>
+            </Nav>
+        </Navbar>
+    );
 };
 
 export default NavBar;
