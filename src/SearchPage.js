e2c11bc3f4e86bb243e924a65b7311e379ac7980
Agnes <agnes.mccall5@gmail.com>
2024-09-26 21:43:31 -0400

    add basic search form

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { React, useState } from "react";
 
 const SearchPage = () => {
-    return <div>SearchPage</div>;
+    const [search, setSearch] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(search);
+        setSearch("");
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit}>
+            <input type="text" 
+                placeholder="Search..." 
+                value={search} 
+                onChange={(e) => setSearch(e.target.value)}/>
+            <button type="submit">Search</button>
+        </form>
+        </>
+    );
 };
 
 export default SearchPage;
