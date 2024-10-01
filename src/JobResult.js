adb1b17d669147e305983c9397583b096469c524
Agnes <agnes.mccall5@gmail.com>
2024-09-30 22:07:45 -0400

    add company name to jobResult

--- src/JobResult.js
+++ src/JobResult.js
@@ -5,6 +5,7 @@ const JobResult = ({ result }) => {
     return (
         <div className="jobresult">
             <h3>{result.title}</h3>
+            <p>at {result.companyName}</p>
             <div>
                 <p><b>Salary:</b> {result.salary ? `$${result.salary.toLocaleString()}` : "None specified"}</p>
                 <p><b>Equity:</b> {result.equity ? `${result.equity} %` : "None"}</p>
