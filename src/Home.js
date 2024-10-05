94e874057b7691a29308d47b794be4fae19e1d9b
Agnes <agnes.mccall5@gmail.com>
2024-10-04 20:50:00 -0400

    add conditonal output on homepage when logged in

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,16 @@
-import React from "react";
+import {React, useContext} from "react";
 import './Home.css';
+import { UserContext } from "./UserContext";
 
 const Home = () => {
+    const { user, setUser } = useContext(UserContext);
+
     return (
         <div className="home">
             <h1>Jobly</h1>
             <p>Look for a job. Get a job.</p>
-            <p>Welcome back</p>
+            { user &&
+            <p>Welcome back, {user.username}!</p>}
         </div>
     );
 };
