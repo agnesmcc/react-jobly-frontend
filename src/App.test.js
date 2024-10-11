1c56642c9e127969e233e5b80f88b661cd7dfb43
Agnes <agnes.mccall5@gmail.com>
2024-10-10 21:26:59 -0400

    Add test for Home page

--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,6 @@ import { MemoryRouter } from 'react-router-dom';
 import { JobsProvider } from './JobsContext';
 import { UserProvider } from './UserContext';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test('renders App using MemoryRouter', () => {
   const { container } = render(
     <MemoryRouter initialEntries={['/']}>
